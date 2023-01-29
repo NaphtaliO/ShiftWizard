b38499f642267d7334c38449e3f7a778017a2059
Naphtali <naphtali2003@gmail.com>
2023-01-29 03:25:22 +0000

    Updated models and added new routes

--- backend/Models.py
+++ backend/Models.py
@@ -1,6 +1,5 @@
 from app import db
 
-
 class Organisation(db.Model):
     __tablename__ = "organisations"
 
@@ -11,14 +10,6 @@ class Organisation(db.Model):
     password = db.Column(db.String(100))
     employees = db.relationship('Employee', backref='organisation')
 
-    def __init__(self, id, name, address, email, password):
-        self.id = id
-        self.name = name
-        self.address = address
-        self.email = email
-        self.password = password
-
-
 class Employee(db.Model):
     __tablename__ = "employees"
     id = db.Column(db.String(100), primary_key=True, unique=True)
@@ -28,31 +19,65 @@ class Employee(db.Model):
     password = db.Column(db.String(100))
     organisation_id = db.Column(db.String(100), db.ForeignKey(
         'organisations.id'), nullable=False)
+    roster_id = db.Column(db.String(100), db.ForeignKey(
+        "rosters.id"))
     shifts = db.relationship('Shift', backref='employee')
 
+    def __init__(self, id, name, job, email, roster_id, organisation_id):
+        self.id = id
+        self.name = name
+        self.job = job
+        self.roster_id = roster_id
+        self.email = email,
+        self.organisation_id = organisation_id
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "job": self.job,
+            "email": self.email,
+            "shifts": [shift.to_dict() for shift in self.shifts]
+        }
 
 class Shift(db.Model):
     __tablename__ = "shifts"
-    id = db.Column(db.String(100), primary_key=True)
+    id = db.Column(db.String(100), primary_key=True, unique=True)
     description = db.Column(db.String(100))
     day = db.Column(db.String(100), nullable=False)
-    start_time = db.Column(db.Time, nullable=False)
-    end_time = db.Column(db.Time, nullable=False)
+    start_time = db.Column(db.String(100), nullable=False)
+    end_time = db.Column(db.String(100), nullable=False)
     employee_id = db.Column(db.String(100), db.ForeignKey(
         'employees.id'), nullable=False)
 
+    def __init__(self, description, day, start_time, end_time, employee_id):
+        self.description = description
+        self.day = day
+        self.start_time = start_time
+        self.end_time = end_time
+        self.employee_id = employee_id
+
+    def to_dict(self):
+        return {
+            "description": self.description,
+            "day": self.day,
+            "startTime": self.start_time,
+            "endTime": self.end_time
+        }
 
 class Roster(db.Model):
     __tablename__ = "rosters"
-    id = db.Column(db.String(100), primary_key=True)
+    id = db.Column(db.String(100), primary_key=True, unique=True)
     name = db.Column(db.String(100))
-    employees = db.relationship('Employee', secondary='employee_roster', lazy='subquery', backref=db.backref('rosters', lazy=True))
+    employees = db.relationship('Employee', backref='roster')
 
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
-class EmployeeRoster(db.Model):
-    __tablename__ = "employee_roster"
-    id = db.Column(db.String(100), primary_key=True)
-    employee_id = db.Column(db.String(100), db.ForeignKey(
-        'employees.id'), nullable=False)
-    roster_id = db.Column(db.String(100), db.ForeignKey(
-        'rosters.id'), nullable=False)
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "employees": [employee.to_dict() for employee in self.employees]
+        }
--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from bcrypt import gensalt, hashpw
+from bcrypt import gensalt, hashpw, checkpw
 import uuid
 from createToken import create_jwt
 
@@ -14,29 +14,32 @@ db = SQLAlchemy()
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://naphtali:naphtali123@mydb.cg1kk4ysnwdb.eu-west-1.rds.amazonaws.com:3306/ebdb"
 db.init_app(app)
 
-# This is the db model for Organisations you can add or edit later
+from Models import *
+# # This is the db model for Organisations you can add or edit later
+# class Organisation(db.Model):
+#     __tablename__ = "organisations"
 
+#     id = db.Column(db.String(100), primary_key=True, unique=True)
+#     name = db.Column(db.String(100))
+#     address = db.Column(db.String(100))
+#     email = db.Column(db.String(70), unique=True)
+#     password = db.Column(db.String(80))
 
-class Organisation(db.Model):
-    __tablename__ = "organisations"
+#     def __init__(self, id, name, address, email, password):
+#         self.id = id
+#         self.name = name
+#         self.address = address
+#         self.email = email
+#         self.password = password
 
-    id = db.Column(db.String(100), primary_key=True, unique=True)
-    name = db.Column(db.String(100))
-    address = db.Column(db.String(100))
-    email = db.Column(db.String(70), unique=True)
-    password = db.Column(db.String(80))
-
-    def __init__(self, id, name, address, email, password):
-        self.id = id
-        self.name = name
-        self.address = address
-        self.email = email
-        self.password = password
+#This will create all tables in our database
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def index():
-    return "<h1>Hwllo World!!!<h1>"
+    return "<h1>Hello World!!!<h1>"
 
 
 @app.route("/api/organisation/signup", methods=['POST'])
@@ -91,8 +94,8 @@ def login():
             #Comapring passwords
             hashedPassword = organisation.password.encode('utf-8')  # Encode strings before hashing
             password = password.encode('utf-8') #Encode strings before hashing
-            if hashpw(password, hashedPassword) == hashedPassword:
-                token= create_jwt(organisation.id) #Create toke and send it to the user
+            if checkpw(password, hashedPassword):
+                token=create_jwt(organisation.id) #Create token and send it to the user
                 return jsonify(id=organisation.id, name=organisation.name, address=organisation.address, email=organisation.email, token=token), 201
             else:
                 response = {"message" : "Incorrect Password"}
@@ -108,6 +111,101 @@ def login():
         return jsonify(response), 400
     
 
+@app.route("/api/employee/create", methods=['POST'])
+def create_employee():
+    data = request.get_json()
+    name, email,password, job, organisation_id = data.get(
+        'name'), data.get('email'), data.get('password'), data.get('job'), data.get('organisation_id')
+    try:
+        employee = Employee(
+            id=str(uuid.uuid4()),
+            name=name,
+            job=job,
+            email=email,
+            password=password,
+            organisation_id=organisation_id
+        )
+        db.session.add(employee)
+        db.session.commit()
+        return jsonify(id=employee.id, name=employee.name, job=employee.job, email=employee.email,
+                        organisation_id=employee.organisation_id), 200
+    except Exception as e:
+        print(e)
+        response = {"message": "An error Occurred. Try Again"}
+        return jsonify(response), 400
+
+@app.route("/api/roster/create", methods=['POST'])
+def create_roster():
+    data = request.get_json()
+    name = data.get('name')
+    try:
+        roster = Roster(
+            id = str(uuid.uuid4()),
+            name=name
+        )
+        db.session.add(roster)
+        db.session.commit()
+        return jsonify(id=roster.id, name=roster.name), 200
+    except Exception as e:
+        print(e)
+        response = {"message": "An error Occurred. Try Again"}
+        return jsonify(response), 400
+
+
+@app.route("/api/roster/addEmployee", methods=['POST'])
+def add_employee_to_roster():
+    data = request.get_json()
+
+    roster_id, employee_id = data.get('roster_id'), data.get('employee_id')
+    #query roster by id
+    try:
+        roster = db.session.execute(db.select(Roster).where(Roster.id == roster_id)).scalar()
+        employee = db.session.execute(db.select(Employee).where(Employee.id == employee_id)).scalar()
+        roster.employees.append(employee)
+        db.session.commit()
+        return jsonify(messsage = "hello"), 200
+    except Exception as e:
+        print(e)
+        response = {"message": "An error Occurred. Try Again"}
+        return jsonify(response), 400
+    
+
+@app.route("/api/roster/view", methods=['POST'])
+def roster():
+    data = request.get_json()
+
+    roster_id = data.get('roster_id')
+    # query roster by id
+    try:
+        roster = Roster.query.options(
+            db.joinedload('employees', innerjoin=True).joinedload('shifts')
+        ).filter_by(id=roster_id).first()
+        
+        roster_dict = {
+            "id": roster.id,
+            'name': roster.name,
+            'employees': [employee.to_dict() for employee in roster.employees]
+        }
+
+        print(roster_dict)
+
+        #print(vars(roster))
+        return jsonify(messsage="hello"), 200
+    except Exception as e:
+        print(e)
+        response = {"message": "An error Occurred. Try Again"}
+        return jsonify(response), 400
+    
+# @app.route("/api/roster/addShift", methods=["POST"])
+# def addShift():
+#     data = request.get_json()
+
+#     try:
+#         pass
+#     except Exception as e:
+#         print(e)
+#         response = {"message": "An error Occurred. Try Again"}
+#         return jsonify(response), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
